78bcda77ef2d76fd7da98405de63fa911fcda136
Nicolas Pouillon <nipo@ssji.net>
2025-10-07 14:36:43 +0200

    pixel_led: Rework implementation

--- src/pixel_led/pixel_led.c
+++ src/pixel_led/pixel_led.c
@@ -15,22 +15,47 @@ static struct pr_pixel_led_strip *g_instance[2][4] = {};
 static
 void on_pixel_led_irq(struct pr_pixel_led_strip *strip)
 {
+    bool done = false;
+
     while (!pio_sm_is_tx_fifo_full(strip->pio, strip->sm)) {
         if (strip->offset < strip->pixel_count) {
             uint32_t val = strip->pixel_data[strip->offset++] << 8;
             if (strip->inv)
                 val = ~val;
+
             pio_sm_put(strip->pio, strip->sm, val);
         } else {
             pio_sm_put(strip->pio, strip->sm, 0);
-            pio_set_irq0_source_mask_enabled(
-                strip->pio,
-                (1 << (PIO_INTR_SM0_TXNFULL_LSB + strip->sm)),
-                false);
 
-            pr_task_exec_in_us(&strip->done_task, 50);
-
+            done = true;
             break;
+        }
+    }
+
+    if (done) {
+        pr_task_exec_in_us(&strip->done_task, 50);
+
+        pio_set_irq0_source_enabled(
+            strip->pio,
+            pio_get_tx_fifo_not_full_interrupt_source(strip->sm),
+            false);
+    } else {
+        pio_set_irq0_source_enabled(
+            strip->pio,
+            pio_get_tx_fifo_not_full_interrupt_source(strip->sm),
+            true);
+    }
+}
+
+static
+void on_pio_irq(uint8_t pio_index, PIO pio)
+{
+    for (uint8_t sm = 0; sm < 4; ++sm) {
+        struct pr_pixel_led_strip *strip = g_instance[pio_index][sm];
+
+        pio_interrupt_clear(pio, pio_get_tx_fifo_not_full_interrupt_source(sm));
+        if (strip) {
+            on_pixel_led_irq(strip);
         }
     }
 }
@@ -38,39 +63,13 @@ void on_pixel_led_irq(struct pr_pixel_led_strip *strip)
 static
 void on_pio0_irq0(void)
 {
-    uint8_t pio_index = 0;
-    PIO pio = pio_index ? pio1 : pio0;
-    
-    for (uint8_t sm = 0; sm < 4; ++sm) {
-        struct pr_pixel_led_strip *strip = g_instance[pio_index][sm];
-
-        if (strip)
-            on_pixel_led_irq(strip);
-        else
-            pio_set_irq0_source_mask_enabled(
-                pio,
-                (1 << (PIO_INTR_SM0_TXNFULL_LSB + sm)),
-                false);
-    }
+    on_pio_irq(0, pio0);
 }
 
 static
 void on_pio1_irq0(void)
 {
-    uint8_t pio_index = 1;
-    PIO pio = pio_index ? pio1 : pio0;
-
-    for (uint8_t sm = 0; sm < 4; ++sm) {
-        struct pr_pixel_led_strip *strip = g_instance[pio_index][sm];
-    
-        if (strip)
-            on_pixel_led_irq(strip);
-        else
-            pio_set_irq0_source_mask_enabled(
-                pio,
-                (1 << (PIO_INTR_SM0_TXNFULL_LSB + sm)),
-                false);
-    }
+    on_pio_irq(1, pio1);
 }
 
 static
@@ -82,6 +81,7 @@ void on_refresh(struct pr_task *task)
     
     if (strip->refreshing)
         return;
+    strip->refreshing = true;
 
     for (size_t i = 0; i < strip->pixel_count; ++i) {
         if ((i & 0x7) == 0)
@@ -90,12 +90,11 @@ void on_refresh(struct pr_task *task)
     }
     dprintf("\n");
 
-    strip->refreshing = true;
     strip->dirty = false;
     strip->offset = 0;
-    pio_set_irq0_source_mask_enabled(
+    pio_set_irq0_source_enabled(
         strip->pio,
-        (1 << (PIO_INTR_SM0_TXNFULL_LSB + strip->sm)),
+        pio_get_tx_fifo_not_full_interrupt_source(strip->sm),
         true);
 }
 
@@ -125,7 +124,6 @@ void pr_pixel_led_strip_init(struct pr_pixel_led_strip *strip,
     
     strip->pio = pio_index ? pio1 : pio0;
     strip->sm = sm;
-    int irq = (pio_index ? PIO1_IRQ_0 : PIO0_IRQ_0);
 
     pr_task_init(&strip->refresh_task, queue, on_refresh);
     pr_task_init(&strip->done_task, queue, on_done);
@@ -137,18 +135,20 @@ void pr_pixel_led_strip_init(struct pr_pixel_led_strip *strip,
 
     strip->pixel_count = led_count;
     strip->offset = 0;
+    strip->inv = inv;
 
     g_instance[pio_index][strip->sm] = strip;
 
     if (sm == 0) {
         pixel_led_pio_init(strip->pio);
-        pio_set_irq0_source_mask_enabled(strip->pio, -1, false);
-        irq_add_shared_handler(irq,
-                               pio_index ? on_pio1_irq0 : on_pio0_irq0,
-                               PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
     }
 
-    strip->inv = inv;
+    
+    pio_set_irq0_source_enabled(
+        strip->pio,
+        pio_get_tx_fifo_not_full_interrupt_source(strip->sm),
+        false);
+
     pixel_led_sm_init(strip->pio, strip->sm, pin_data, inv);
     pio_sm_restart(strip->pio, sm);
     pio_sm_set_enabled(strip->pio, sm, true);
@@ -156,7 +156,13 @@ void pr_pixel_led_strip_init(struct pr_pixel_led_strip *strip,
     pr_task_exec(&strip->refresh_task);
 
     if (sm == 0) {
-        user_irq_claim(irq);
+        int irq = pio_index ? PIO1_IRQ_0 : PIO0_IRQ_0;
+
+        /* irq_add_shared_handler(irq, */
+        /*                        pio_index ? on_pio1_irq0 : on_pio0_irq0, */
+        /*                        PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY); */
+        irq_set_exclusive_handler(irq,
+                                  pio_index ? on_pio1_irq0 : on_pio0_irq0);
         irq_set_enabled(irq, 1);
     }
 }
